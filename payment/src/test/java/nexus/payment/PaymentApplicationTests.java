13f9a927d16d9511508a9f83554a5365ad462a58
MateusdiSousa <mateus69107@gmail.com>
2025-08-22 15:29:36 -0300

    :tada: Starting the project

---
+++ payment/src/test/java/nexus/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package nexus.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
