6d7759d888e72e5884516d1e1d3a2ff26cb64136
MateusdiSousa <mateus69107@gmail.com>
2025-08-27 15:05:19 -0300

    feat: Creating DTOs

---
+++ inventory/src/main/java/nexus/inventory/dto/BookDto.java
@@ -0,0 +1,15 @@
+package nexus.inventory.dto;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public record BookDto(
+    String title,
+    String autor,
+    String publisher,
+    Integer publishYear,
+    BigInteger price,
+    Long stock,
+    List<String> listCodeCategory,
+    Long bookCode
+) {}
