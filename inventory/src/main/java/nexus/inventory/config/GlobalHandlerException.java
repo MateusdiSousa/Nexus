7396d36a0c76af624c088bc312484700f45a97fe
MateusdiSousa <mateus69107@gmail.com>
2025-08-31 18:54:18 -0300

    feat: Adding global handler exception

---
+++ inventory/src/main/java/nexus/inventory/config/GlobalHandlerException.java
@@ -0,0 +1,30 @@
+package nexus.inventory.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import nexus.inventory.exceptions.NotFoundException;
+import nexus.inventory.exceptions.ConflictException;
+import nexus.inventory.exceptions.BadRequestException;
+
+@ControllerAdvice
+public class GlobalHandlerException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(exception = NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(exception = ConflictException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(exception = BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
